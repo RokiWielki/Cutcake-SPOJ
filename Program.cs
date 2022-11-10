920521783a973acd3a6d939c115d13aecb2a645e
RokiWielki <Cruck222@interia.pl>
2022-11-10 09:16:29 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace Cutcake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ans=new StringBuilder();
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                int N = int.Parse(Console.ReadLine());
+                ans.AppendLine(Cut(N).ToString());  
+            }
+            Console.WriteLine(ans.ToString());
+            
+            
+        }
+        static int Cut(int N)
+        {
+            int i = 3,X;
+            
+            if (N <= 1) { return 0; }
+            if (N == 2) { return 1; }
+            else
+            {
+                X = 2;
+                while (i <= N) 
+                    {
+                        i += X;
+                        if (i > N)
+                        {
+                            return X;
+                        }
+                        X++;
+                    }
+                return X;
+            }  
+        }
+    }
+}
+
+
+
